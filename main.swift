9c7f9512c8352fda7410f65a33bffe341e7f3684
MaryamH27 <maryam.rhasnath09@gmail.com>
2024-07-11 15:32:15 -0400

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,20 @@
+var rating = 4.2
+var reccomendation: String
+
+if rating >= 4.5 {
+reccomendation = "Highly Reccomended"
+print(reccomendation)
+}
+else if rating
+ >= 3.5 &&  rating <= 4.4 {
+    reccomendation = "Reccomended"
+    print(reccomendation)
+}
+else if rating >= 2.5 && rating <= 3.5 {
+    reccomendation = "Moderately reccomended"
+    print(reccomendation)
+} else if rating <= 2.5 {
+    reccomendation = "not reccomended"
+    print(reccomendation)
+    }
+
